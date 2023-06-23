f42d04e34206d04d2fbea9502adbd325023253bc
Nastya Provotorova <nastos.workspace@gmail.com>
2023-06-23 15:49:23 +0300

    Осталось оформление

---
+++ naughty_children/error_log/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ErrorLogConfig(AppConfig):
+    name = 'error_log'
---
+++ naughty_children/error_log/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    """Тема, на которую педагог пишет отчёт."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class Entry(models.Model):
+    """Отчёт преподавателя об инциденте."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return f'{self.text[:50]}...'
